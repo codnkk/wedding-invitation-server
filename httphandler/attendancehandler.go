61582b00a842b4491cae1557ed146f05416d8c62
codnkk <codnkk@gmail.com>
2026-09-09 20:05:53 +0900

    Update attendancehandler.go

--- httphandler/attendancehandler.go
+++ httphandler/attendancehandler.go
@@ -4,8 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/juhonamnam/wedding-invitation-server/sqldb"
-	"github.com/juhonamnam/wedding-invitation-server/types"
+	"github.com/codnkk/wedding-invitation-server/sqldb"
+	"github.com/codnkk/wedding-invitation-server/types"
 )
 
 type AttendanceHandler struct {
