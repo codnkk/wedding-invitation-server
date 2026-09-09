7db694a368b0897a04c0d47dc84ca5cd8f80e038
codnkk <codnkk@gmail.com>
2026-09-09 20:04:58 +0900

    Update app.go

--- app.go
+++ app.go
@@ -4,9 +4,9 @@ import (
 	"database/sql"
 	"net/http"
 
-	"github.com/juhonamnam/wedding-invitation-server/env"
-	"github.com/juhonamnam/wedding-invitation-server/httphandler"
-	"github.com/juhonamnam/wedding-invitation-server/sqldb"
+	"github.com/codnkk/wedding-invitation-server/env"
+	"github.com/codnkk/wedding-invitation-server/httphandler"
+	"github.com/codnkk/wedding-invitation-server/sqldb"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/rs/cors"
 )
