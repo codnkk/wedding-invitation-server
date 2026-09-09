29bf7a69d65a8da3d267259b0e7b051ff7d8c379
codnkk <codnkk@gmail.com>
2026-09-09 20:06:05 +0900

    Update guestbookhandler.go

--- httphandler/guestbookhandler.go
+++ httphandler/guestbookhandler.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/juhonamnam/wedding-invitation-server/sqldb"
-	"github.com/juhonamnam/wedding-invitation-server/types"
+	"github.com/codnkk/wedding-invitation-server/sqldb"
+	"github.com/codnkk/wedding-invitation-server/types"
 )
 
 type GuestbookHandler struct {
